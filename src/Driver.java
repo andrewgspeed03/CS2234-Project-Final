e5f7d5cdfa3f1a7666e76d63eda6f92a14163378
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-05-09 17:52:48 -0500

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,142 +1,252 @@
-import org.openstreetmap.gui.jmapviewer.*;
-import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
-import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
-import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
-import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
-
-import java.awt.BasicStroke;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.AffineTransformOp;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
+import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
+
+import java.math.*;
+
+
+
+/**
+ * 
+ * @author good0161
+ * @version 5.0.1
+ * Designs and creates an executable jar file to display the entire trip on a tripMap
+ */
 
 public class Driver {
 	
-	public static int animationSec;
-	public static ArrayList<TripPoint> trip;
-	public static BufferedImage raccoon;
-
+	// Declare class data
+	private static JFrame proFrame;
+	private static JPanel topPanel;
+	private static JButton play;
+	private static JCheckBox enStop;
+	private static JComboBox<String> aniTime;
+	private static JMapViewer tripMap;
+	private static int speed = 15;
+	private static boolean showStops = false;
+	private static Timer timer;
+	private static BufferedImage marker = null;
+	private static int numStops;
+	private static AffineTransform rotate = new AffineTransform();
+	
+	
     public static void main(String[] args) throws FileNotFoundException, IOException {
-    	TripPoint.readFile("triplog.csv");
-    	TripPoint.h2StopDetection();
-    	raccoon = ImageIO.read(new File("raccoon.png"));
-    	
-    	// set up frame
-        JFrame frame = new JFrame("Map Viewer");
-        frame.setLayout(new BorderLayout());
-        
-        // set up top panel for input selections
-        JPanel topPanel = new JPanel();
-        frame.add(topPanel, BorderLayout.NORTH);
-        // play button
-        JButton play = new JButton("Play");
-        // checkbox to enable/disable stops
-        JCheckBox includeStops = new JCheckBox("Include Stops");
-        // dropbox to pick animation time
-        String[] timeList = {"Animation Time", "15", "30", "60", "90"};
-        JComboBox<String> animationTime = new JComboBox<String>(timeList);
-        animationSec = 0;
-        // add all to top panel
-        topPanel.add(animationTime);
-        topPanel.add(includeStops);
-        topPanel.add(play);
-        
-        // set up mapViewer
-        JMapViewer mapViewer = new JMapViewer();
-        frame.add(mapViewer);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mapViewer.setTileSource(new OsmTileSource.TransportMap());
-        
-        // add listeners
-        play.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-                    @Override
-                    protected Void doInBackground() throws Exception {
-                    	mapViewer.removeAllMapMarkers(); // remove all markers from the map
-                    	mapViewer.removeAllMapPolygons();
-                    	if (includeStops.isSelected()) {
-                    		trip = TripPoint.getTrip();
-                    	}
-                    	else {
-                    		trip = TripPoint.getMovingTrip();
-                    	}
-                        plotTrip(animationSec, trip, mapViewer);
-                        return null;
-                    }
-                };
-                worker.execute();
-            }
-        });
-        animationTime.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    Object selectedItem = animationTime.getSelectedItem();
-                    if (selectedItem instanceof String) {
-                        String selectedString = (String) selectedItem;
-                        if (!selectedString.equals("Animation Time")) {
-                            animationSec = Integer.parseInt(selectedString);
-                            System.out.println("Updated to " + animationSec);
-                        }
-                    }
-                }
-            }
-        });
 
-        // set the map center and zoom level
-        mapViewer.setDisplayPosition(new Coordinate(34.82, -107.99), 6);
+    	// Read file and call stop detection
+    	
+    	TripPoint.readFile("triplog.csv");
+    	numStops = TripPoint.h2StopDetection();
+    	ArrayList<TripPoint> trip = TripPoint.getTrip();
+    	ArrayList<TripPoint> stops = new ArrayList<>(trip);
+    	ArrayList<TripPoint> movingTrip = TripPoint.getMovingTrip();
+    	stops.removeAll(movingTrip);
+    	marker = ImageIO.read(new File("arrow.png"));
+    	
+    	// Set up frame, include your name in the title
+    	proFrame = new JFrame("Project 5 - Andrew Goodspeed");
+        proFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        proFrame.setSize(1920, 1080);
+        proFrame.setLayout(new BorderLayout());
+    
+        // Set up Panel for input selections
+        topPanel = new JPanel();
+    	
+        // Play Button
+        play = new JButton("Play");
+    	
+        // CheckBox to enable/disable stops
+        enStop = new JCheckBox("Include Stops");
+
+        // ComboBox to pick animation time
+        String[] Times = {"Animation Time","15", "30", "60", "90"};
+        aniTime = new JComboBox<String>(Times);
+        aniTime.setSelectedItem(0);
         
+    	aniTime.setEditable(false);
+    	
+        // Add all to top panel
+        topPanel.add(play);
+        topPanel.add(enStop);
+        topPanel.add(aniTime);
+        proFrame.add(topPanel, BorderLayout.NORTH);
+        
+        // Set up tripMap
+        tripMap = new JMapViewer();
+        tripMap.setTileSource(new OsmTileSource.TransportMap());
+        
+        proFrame.add(tripMap, BorderLayout.CENTER);
+        
+        // Add listeners for GUI components
+        play.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		if(timer != null) 
+        			timer.stop();
+				Play(trip,stops, movingTrip);
+
+        	}
+
+        });
+        enStop.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		if(enStop.isSelected())
+        			showStops = true;
+        		else
+        			showStops = false;
+        	}
+        });
+        aniTime.addItemListener(new ItemListener() {
+        	@Override
+        	public void itemStateChanged(ItemEvent e) {
+        		if(e.getStateChange() == ItemEvent.SELECTED) {
+        			String sel = (String) aniTime.getSelectedItem();
+        			if(aniTime.getSelectedIndex() != 0) {
+        				speed = Integer.parseInt(sel);
+        			}        			
+        		}
+        	}
+        });
+        
+
+        // Set the tripMap center and zoom level
+        tripMap.setDisplayPosition(new Coordinate(35,-110), 6);
+        proFrame.setVisible(true);
     }
-    
-    
-    // plot the given trip ArrayList with animation time in seconds
-    public static void plotTrip(int seconds, ArrayList<TripPoint> trip, JMapViewer map) throws IOException {
-    	// amount of time between each point in milliseconds
-    	long delayTime = (seconds * 1000) / trip.size();
+    // Animate the trip based on selections from the GUI components
+
+
+    private static void Play(ArrayList<TripPoint> Trip, ArrayList<TripPoint> Stops, ArrayList<TripPoint> MovingTrip){
     	
-    	Coordinate c1;
-    	Coordinate c2 = null;
-    	MapMarker marker;
-    	MapMarker prevMarker = null;
-    	MapPolygonImpl line;
+    	tripMap.removeAllMapMarkers();
+    	tripMap.removeAllMapPolygons();
     	
-    	for (int i = 0; i < trip.size(); ++i) {
-    		c1 = new Coordinate(trip.get(i).getLat(), trip.get(i).getLon());
-    		marker = new IconMarker(c1, raccoon);
-            
-    		map.addMapMarker(marker);
-    		if (i != 0) {
-    			c2 = new Coordinate(trip.get(i-1).getLat(), trip.get(i-1).getLon());
-    		}
-    		if (c2 != null) {
-    			line = new MapPolygonImpl(c1, c2, c2);
-    			line.setColor(Color.RED);
-    			line.setStroke(new BasicStroke(3));
-    			map.addMapPolygon(line);
-    			map.removeMapMarker(prevMarker);
-    		}
-    		try {
-				Thread.sleep(delayTime);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-    		prevMarker = marker;
+    	List<Coordinate> line = new ArrayList<Coordinate>();
+    	List<Coordinate> dot = new ArrayList<Coordinate>();
+    	
+    	
+    	
+    	Graphics g2d = (Graphics2D) proFrame.getGraphics();
+    	g2d.setColor(Color.RED);
+    	//Graphics2D img = marker.createGraphics();
+    	int period = 1000;
+    		
+    	for(TripPoint y: Stops)
+    		dot.add(new Coordinate(y.getLat(),y.getLon()));
+    	
+    	final int[] current = {0};
+    	final int[] stopCount = {0};
+    	final MapMarker[] prev = new MapMarker[1];
+    	
+    	if(showStops) {
+    		for(TripPoint x: Trip) 
+        		line.add(new Coordinate(x.getLat(),x.getLon()));
+    		period = (speed * 1000) / (Trip.size());
     	}
-    	
+    	else
+    		for(TripPoint x: MovingTrip) 
+        		line.add(new Coordinate(x.getLat(),x.getLon()));
+    		period =  (speed * 1000) / (line.size());
+
+    	timer = new Timer(period, new ActionListener() {
+    			@Override
+    			public void actionPerformed(ActionEvent e) {
+    				
+    				if(current[0] < line.size()-1) {
+    					Coordinate from = line.get(current[0]);
+    					Coordinate to = line.get(current[0] + 1);
+    					
+    					double toa = (to.getLat() - from.getLat()) / (to.getLon() - from.getLon());
+    					BufferedImage reset = marker;
+    					marker = rotate(marker, Math.toRadians(Math.atan(toa)));
+
+    					
+	    		    	MapPolygonImpl line = new MapPolygonImpl(from,from ,to);
+	    		    	MapMarker pnt = new IconMarker(to , marker);
+	    		    	marker = reset;
+	    		    	
+	    		    	line.setColor(Color.RED);
+	    		    	tripMap.removeMapMarker(prev[0]);
+	    		    	tripMap.addMapMarker(pnt);
+	    		    	tripMap.addMapPolygon(line);
+	    		    	prev[0] = pnt;
+	    		    	if(showStops && stopCount[0] < numStops) { 
+	    		    		Coordinate stop = dot.get(stopCount[0]);
+	    		    		if(to.equals(stop)){
+	    		    			MapMarkerDot stopMark = new MapMarkerDot(stop);
+	    		    			stopMark.setBackColor(Color.RED);
+	    		    			stopMark.setColor(Color.RED);
+	    		    			
+	    		    			tripMap.addMapMarker(stopMark);
+	    		    			stopCount[0]++;
+	    		    		}
+	    		    	}
+	    		    	
+    				}
+    				
+    				current[0]++;
+    				
+    			}
+    	});
+    	timer.setInitialDelay(0);
+    	timer.start();
     }
-    
+    public static BufferedImage rotate(BufferedImage img, Double angle)
+    {
+ 
+        // Getting Dimensions of image
+        int width = img.getWidth();
+        int height = img.getHeight();
+ 
+        // Creating a new buffered image
+        BufferedImage newImage = img;
+ 
+        // creating Graphics in buffered image
+        Graphics2D g2 = newImage.createGraphics();
+ 
+        // Rotating image by degrees using toradians()
+        // method
+        // and setting new dimension t it
+        g2.rotate(angle);
+        //rotate.rotate(angle);
+        AffineTransformOp op = new AffineTransformOp(rotate ,null);
+        g2.drawImage(img, op, 0, 0);
+ 
+        // Return rotated buffer image
+        return newImage;
+    }
+ 
+   
+   
 }
